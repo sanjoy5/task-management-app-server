19b235770e582cf775f38b835268bb38277c717d
Sanjoy Sarker <sarkerd52@gmail.com>
2023-11-05 01:19:28 +0600

    jwt verification and register user done

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware 
 app.use(cors())
 app.use(express.json())
+
+// 2nd Task - jwt middleware
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    // bearer token 
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 
 
@@ -27,8 +46,34 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
-
         const tasksCollection = client.db('taskDB').collection('task')
+        const usersCollection = client.db('taskDB').collection('users')
+
+
+        // 1st Task - jwt token
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+            res.send({ token })
+        })
+
+
+        // Users Collection 
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existUser = await usersCollection.findOne(query)
+            if (existUser) {
+                return res.send({ message: 'User Already exists' })
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+        // Task Collection 
 
         app.get('/tasks', async (req, res) => {
             const result = await tasksCollection.find().sort({ createdAt: -1 }).toArray()
@@ -42,6 +87,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/delete-task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await tasksCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
